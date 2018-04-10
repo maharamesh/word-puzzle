be7c9d81443b72a3355f42fb8cc35f1722452763
MAHAJAN\Mahajan A <maha.199536@gmail.com>
2018-04-10 22:18:12 +0530

    animated splash screen Added.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,20 +5,22 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 import { Keyboard } from '@ionic-native/keyboard';
 
 import { TabsPage } from '../pages/tabs/tabs';
+import { timer } from 'rxjs/observable/timer';
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
   rootPage:any = TabsPage;
- 
+  showSplash = true;
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen,public key:Keyboard) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       splashScreen.hide();
       statusBar.styleDefault();
-      key.disableScroll(true)
+      key.disableScroll(true);
+        timer(3000).subscribe(() => this.showSplash = false) // <-- hide animation after 3s
     });
   }
 }
