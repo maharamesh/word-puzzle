4c205c4c0af59924aa105e768722f18f095e6ffc
SRIRAM\Sriram <sriram.vjs@gmail.com>
2018-04-21 23:12:43 +0530

    Register back button issues fixed and added a background  transparent image added. - Sriram

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,6 +13,7 @@ import { timer } from 'rxjs/observable/timer';
 export class MyApp {
   rootPage: any = TabsPage;
   showSplash = true;
+  popupAlert = false;
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, public key: Keyboard, public actionSheetCtrl: ActionSheetController, public app: App) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
@@ -24,9 +25,13 @@ export class MyApp {
 
       //Registration of push in Android and Windows Phone
       platform.registerBackButtonAction(() => {
+
+        if(this.popupAlert==false){
+          this.popupAlert=true; 
         let nav = this.app.getActiveNav();
         if (nav.canGoBack()) { //Can we go back?
           nav.pop();
+          this.popupAlert=false;
         } else {
           let actionSheet = this.actionSheetCtrl.create({
             title: 'Really wanna Exit ?',
@@ -35,18 +40,21 @@ export class MyApp {
                 text: 'Yes',
                 handler: () => {
                   platform.exitApp(); //Exit from app
+                  this.popupAlert=false;
                 }
               }, {
                 text: 'Cancel',
                 role: 'cancel',
                 handler: () => {
                   console.log('Cancel clicked');
+                  this.popupAlert=false;
                 }
               }
             ]
           });
           actionSheet.present();
         }
+      }
       });
     });
   }
